88431bc340064afbb1bceb42ac708908d07a4bd2
MonikaSko <skonecznamonika@gmail.com>
2022-03-25 23:06:31 +0100

    a huge commit

---
+++ oxygen-webhelp/app/search/index/keywords.js
@@ -0,0 +1,18 @@
+define(function() {var keywords=[{w:"Context",p:["p0"]},{w:"Sensitive",p:["p0"]},{w:"Help",p:["p0"]},{w:"It",p:["p1"]},{w:"is",p:["p1"]},{w:"nice",p:["p1"]},{w:"to",p:["p1"]},{w:"have",p:["p1"]},{w:"a",p:["p1","p3","p9"]},{w:"glossary",p:["p1"]},{w:"my",p:["p2","p4","p5","p6","p7","p8"]},{w:"third",p:["p2","p8"]},{w:"reference",p:["p2","p4","p5"]},{w:"file",p:["p2","p4","p5"]},{w:"let",p:["p3"]},{w:"us",p:["p3"]},{w:"play",p:["p3","p9"]},{w:"bit",p:["p3"]},{w:"first",p:["p4","p6"]},{w:"second",p:["p5","p7"]},{w:"task",p:["p6","p8"]},{w:"tast",p:["p7"]},{w:"So",p:["p9"]},{w:"let\'s",p:["p9"]},{w:"game",p:["p9"]},{w:"Thank",p:["p10"]},{w:"you",p:["p10"]}];
+var ph={};
+ph["p0"]=[0, 1, 2];
+ph["p1"]=[3, 4, 5, 6, 7, 8, 9];
+ph["p2"]=[10, 11, 12, 13];
+ph["p3"]=[14, 15, 16, 8, 17];
+ph["p4"]=[10, 18, 12, 13];
+ph["p5"]=[10, 19, 12, 13];
+ph["p6"]=[10, 18, 20];
+ph["p7"]=[10, 19, 21];
+ph["p8"]=[10, 11, 20];
+ph["p9"]=[22, 23, 16, 8, 24];
+ph["p10"]=[25, 26];
+     return {
+         keywords: keywords,
+         ph: ph
+     }
+});
